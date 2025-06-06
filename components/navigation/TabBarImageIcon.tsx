c54075b067f005439754f18095cc9e661afd4f8f
joaopedev <joaopedev@outlook.com>
2025-06-06 18:27:00 -0300

    Correção do tamanho dos Icones;

--- components/navigation/TabBarImageIcon.tsx
+++ components/navigation/TabBarImageIcon.tsx
@@ -2,24 +2,30 @@ import React from "react";
 import { Image, StyleSheet } from "react-native";
 
 type TabBarImageIconProps = {
-    focused: boolean;
-    activeIcon: any;
-    inactiveIcon: any;
+  focused: boolean;
+  activeIcon: any;
+  inactiveIcon: any;
+  size?: number;
 };
 
-export function TabBarImageIcon({ focused, activeIcon, inactiveIcon }: TabBarImageIconProps) {
-    return (
-        <Image
-            source={focused ? activeIcon : inactiveIcon}
-            style={[styles.icon, { opacity: focused ? 1 : 0.6 }]}
-            resizeMode="contain"
-        />
-    );
+export function TabBarImageIcon({
+  focused,
+  activeIcon,
+  inactiveIcon,
+  size,
+}: TabBarImageIconProps) {
+  return (
+    <Image
+      source={focused ? activeIcon : inactiveIcon}
+      style={[styles.icon, { opacity: focused ? 1 : 0.6, padding: size }]}
+      resizeMode="contain"
+    />
+  );
 }
 
 const styles = StyleSheet.create({
-    icon: {
-        width: 50,
-        height: 50,
-    },
+  icon: {
+    width: 18,
+    height: 18,
+  },
 });
