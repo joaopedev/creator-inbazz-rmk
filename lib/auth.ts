04d105bb7a37971012cbbd03686912ece82cfaca
joaopedev <joaopedev@outlook.com>
2025-06-06 01:04:02 -0300

    Auth desnecessario(até o momento);

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,11 @@
-import { router } from "expo-router";
-import { authStore } from "@/store/authStore";
+// import { router } from "expo-router";
+// import { authStore } from "@/store/authStore";
 
-export const auth = async (checkAuth: () => Promise<void>) => {
-    await checkAuth();
-    if (authStore.getState().isAuthenticated) {
-      router.replace("/album");
-    } else {
-      router.replace("/signin/page");
-    }
-};
+// export const auth = async (checkAuth: () => Promise<void>) => {
+//     await checkAuth();
+//     if (authStore.getState().isAuthenticated) {
+//       router.replace("/album");
+//     } else {
+//       router.replace("/signin/page");
+//     }
+// };
