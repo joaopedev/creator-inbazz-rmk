53e9f87b175f3f9ce949b6a9522fe4e23edff793
joaopedev <joaopedev@outlook.com>
2025-06-06 18:32:51 -0300

    Ajuste para alinhamento do figma, verificação dos dois inputs verificando se ja existe email ou cpf na nossa base de dados;

--- components/common/FormStepSignUp/Step2Form/index.tsx
+++ components/common/FormStepSignUp/Step2Form/index.tsx
@@ -1,7 +1,10 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "expo-router";
 import React from "react";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import RNPickerSelect from "react-native-picker-select";
 import { Step2Data, step2Schema } from "../../../../schemas/step2Schema";
 import { useSignUpStore } from "../../../../store/singUpStore";
 import { FormInput } from "../../../FormInput";
@@ -12,7 +15,12 @@ interface Step2Props {
 }
 
 export const Step2Form = ({ onNext }: Step2Props) => {
-  const { control, handleSubmit, formState: { errors, isValid }, watch } = useForm<Step2Data>({
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isValid },
+    watch,
+  } = useForm<Step2Data>({
     mode: "onChange",
     resolver: zodResolver(step2Schema),
     defaultValues: {
@@ -25,69 +33,153 @@ export const Step2Form = ({ onNext }: Step2Props) => {
     },
   });
   const { setStep2 } = useSignUpStore();
+  const router = useRouter();
+  const pickerSelectStyles = {
+    inputIOS: {
+      fontSize: 14,
+      paddingVertical: 14,
+      paddingHorizontal: 12,
+      borderWidth: 0,
+      borderColor: "#ccc",
+      borderRadius: 8,
+      color: "#000",
+      paddingRight: 40,
+      marginBottom: 0,
+      backgroundColor: "#fff",
+    },
+    inputAndroid: {
+      fontSize: 14,
+      paddingVertical: 12,
+      paddingHorizontal: 12,
+      borderWidth: 0,
+      borderColor: "#ccc",
+      borderRadius: 8,
+      color: "#000",
+      paddingRight: 40,
+      marginBottom: 0,
+      backgroundColor: "#fff",
+    },
+    placeholder: {
+      color: "#999",
+    },
+    IconContainer: {
+      position: "absolute",
+      right: 10,
+      top: "50%",
+      transform: [{ translateY: -10 }],
+    },
+  };
+
+  const handleCancel = () => {
+    router.push("/(not-authenticated)/signin/page");
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Agora, alguns dados pessoais</Text>
-
-      <FormInput label="DDD" name="phoneDDD" placeholder="Insira seu nome" control={control} error={errors.phoneDDD?.message} required />
-      <FormInput label="Telefone" name="phoneNumber" placeholder="Insira seu sobrenome" control={control} error={errors.phoneNumber?.message} required />
-      <FormInput label="Data de nascimento" name="birthDate" placeholder="Insira seu CPF" control={control} error={errors.birthDate?.message} required keyboardType="numeric" />
-      <FormInput label="Gênero" name="gender" placeholder="Defina seu genero" control={control} error={errors.gender?.message} required />
-      <FormInput label="Sobre você" name="aboutYou" placeholder="Fale sobre você" control={control} error={errors.aboutYou?.message} required />
-      <FormInput label="É agenciado?" name="haveAgent" placeholder="Sim?" control={control} error={errors.aboutYou?.message} required />
-      {/* <FormInput
-        label="Crie sua senha"
-        name="password"
-        placeholder="Sua senha deve ter, no mínimo, 6 caracteres"
+      <FormInput
+        paddingTopLabel={20}
+        label="DDD"
+        name="phoneDDD"
+        placeholder="DDD ex: '027'"
         control={control}
-        secureTextEntry={!showPassword}
-        error={errors.password?.message}
-        iconRight={<Ionicons name={showPassword ? "eye" : "eye-off"} size={20} onPress={() => setShowPassword(!showPassword)} />}
+        error={errors.phoneDDD?.message}
         required
       />
       <FormInput
-        label="Confirme sua senha"
-        name="confirmPassword"
-        placeholder="Confirme sua senha"
+        paddingTopLabel={20}
+        label="Telefone"
+        name="phoneNumber"
+        placeholder="(99955-0088)"
         control={control}
-        secureTextEntry={!showConfirmPassword}
-        error={errors.confirmPassword?.message}
-        iconRight={<Ionicons name={showConfirmPassword ? "eye" : "eye-off"} size={20} onPress={() => setShowConfirmPassword(!showConfirmPassword)} />}
+        error={errors.phoneNumber?.message}
         required
       />
-
-      <View style={styles.inlineRow}>
-        <View style={styles.instagramInputContainer}>
-          <FormInput
-            label="Instagram"
-            name="instagram"
-            placeholder="Insira seu instagram"
-            control={control}
-            error={errors.instagram?.message}
-            required
-          />
-        </View>
-        <TouchableOpacity style={styles.validateButton} onPress={validateInstagram}>
-          <Text style={styles.validateButtonText}>Validar</Text>
-        </TouchableOpacity>
-      </View>
-
-      <FormInput label="Tiktok" name="tiktok" placeholder="Insira seu tiktok" control={control} error={errors.tiktok?.message} />
-
-      <Controller
-        name="agreeTerms"
+      <FormInput
+        paddingTopLabel={20}
+        label="Data de nascimento"
+        name="birthDate"
+        placeholder="01/02/1993"
         control={control}
-        rules={{ required: true }}
-        render={({ field: { value, onChange } }) => (
-          <TouchableOpacity style={styles.checkboxRow} onPress={() => onChange(!value)}>
-            <Ionicons name={value ? "checkbox" : "square-outline"} size={20} color="#25399E" />
-            <Text style={styles.checkboxText}>Declaro que li e concordo com os <Text style={styles.link}>Termos de Uso</Text></Text>
-          </TouchableOpacity>
-        )}
+        error={errors.birthDate?.message}
+        required
+        keyboardType="numeric"
       />
-      {errors.agreeTerms && <Text style={styles.error}>{errors.agreeTerms.message}</Text>} */}
-
+      {/* <FormInput
+        paddingTopLabel={20}
+        label="Gênero"
+        name="gender"
+        placeholder="Defina seu genero"
+        control={control}
+        error={errors.gender?.message}
+        required
+      /> */}
+      <Text style={[styles.label, { marginTop: 20 }]}>Gênero *</Text>
+      <View style={styles.pickerWrapper}>
+        <Controller
+          control={control}
+          name="gender"
+          render={({ field: { onChange, value } }) => (
+            <RNPickerSelect
+              onValueChange={onChange}
+              value={value}
+              items={[
+                { label: "Feminino", value: "Feminino" },
+                { label: "Masculino", value: "Masculino" },
+                { label: "Não binário", value: "Não binário" },
+                { label: "Outro", value: "Outro" },
+                { label: "Prefiro não dizer", value: "Prefiro não dizer" },
+              ]}
+              placeholder={{ label: "Selecione seu gênero", value: null }}
+              style={pickerSelectStyles}
+              useNativeAndroidPickerStyle={false} // para que use nosso style
+              Icon={() => (
+                <Ionicons name="chevron-down" size={20} color="#999" />
+              )}
+            />
+          )}
+        />
+      </View>
+      {errors.gender && (
+        <Text style={styles.error}>{errors.gender.message}</Text>
+      )}
+      <FormInput
+        paddingTopLabel={20}
+        label="Sobre você"
+        name="aboutYou"
+        placeholder="Fale sobre você"
+        control={control}
+        error={errors.aboutYou?.message}
+        required
+        multiline
+        textArea
+      />
+      <Text style={[styles.label, { marginTop: 20 }]}>É agenciado? *</Text>
+      <View style={styles.pickerWrapper}>
+        <Controller
+          control={control}
+          name="haveAgent"
+          render={({ field: { onChange, value } }) => (
+            <RNPickerSelect
+              onValueChange={onChange}
+              value={value}
+              items={[
+                { label: "Sim", value: "Sim" },
+                { label: "Não", value: "Não" },
+              ]}
+              placeholder={{
+                label: "Selecione se você é agenciado",
+                value: null,
+              }}
+              style={pickerSelectStyles}
+              useNativeAndroidPickerStyle={false}
+            />
+          )}
+        />
+      </View>
+      {errors.haveAgent && (
+        <Text style={styles.error}>{errors.haveAgent.message}</Text>
+      )}
       <TouchableOpacity
         style={[styles.submitButton, !isValid && styles.disabledButton]}
         disabled={!isValid}
@@ -95,28 +187,53 @@ export const Step2Form = ({ onNext }: Step2Props) => {
       >
         <Text style={styles.submitText}>Avançar</Text>
       </TouchableOpacity>
-
-      <TouchableOpacity style={styles.backButton} onPress={() => setStep2(null)}>
-        <Text style={styles.backButtonText}>Voltar</Text>
-      </TouchableOpacity>
-
-      {/* <TouchableOpacity style={styles.loginRow} onPress={() => router.push("/(not-authenticated)/signin/page")}>
-        <Text style={styles.loginText}>
-          Já tem uma conta? <Text style={styles.link}>Acesse</Text>
-        </Text>
-      </TouchableOpacity> */}
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.cancelButton} onPress={handleCancel}>
+          <Text style={styles.cancelButtonText}>Cancelar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.backButton}
+          onPress={() => setStep2(null)}
+        >
+          <Text style={styles.backButtonText}>Voltar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: { padding: 20, backgroundColor: "white", flex: 1 },
-  title: { fontSize: 20, fontWeight: "bold", textAlign: "left", marginBottom: 10 },
-  subtitle: { fontSize: 14, color: "#666", textAlign: "left", marginBottom: 20 },
-  checkboxRow: { flexDirection: "row", alignItems: "center", marginVertical: 10 },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "left",
+    marginBottom: 10,
+    marginLeft: 4,
+  },
+  subtitle: {
+    fontSize: 14,
+    color: "#666",
+    textAlign: "left",
+    marginBottom: 20,
+  },
+  checkboxRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 10,
+  },
   checkboxText: { marginLeft: 10, fontSize: 14 },
   link: { color: "#25399E", textDecorationLine: "underline" },
-  error: { color: "red", fontSize: 12, marginTop: -8, marginBottom: 8 },
+  error: { color: "red", fontSize: 12, marginTop: -4, marginBottom: 8 },
+  pickerWrapper: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    overflow: "hidden",
+    marginHorizontal: 0,
+    marginBottom: 8,
+    backgroundColor: "#fff",
+  },
   submitButton: {
     backgroundColor: "#25399E",
     paddingVertical: 14,
@@ -135,6 +252,7 @@ const styles = StyleSheet.create({
     borderColor: "#25399E",
     borderRadius: 8,
     paddingVertical: 14,
+    flex: 1,
   },
   backButtonText: {
     color: "#25399E",
@@ -165,5 +283,33 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     height: 48,
+  },
+  label: {
+    fontSize: 14,
+    color: "#000",
+    marginBottom: 6,
+    marginLeft: 4,
+    fontFamily: "Unbounded",
+  },
+  cancelButton: {
+    marginTop: 16,
+    borderWidth: 1,
+    borderColor: "#e53935",
+    borderRadius: 8,
+    paddingVertical: 14,
+    flex: 1,
+    marginLeft: 8,
+    backgroundColor: "#e53935",
+  },
+  cancelButtonText: {
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    gap: 10,
+    marginTop: 20,
   },
 });
