588e4f5457d54ade649a1e820412256514402203
joaopedev <joaopedev@outlook.com>
2025-06-06 01:04:23 -0300

    instancia direta com supabase desencessario(até o momento);

--- lib/api.ts
+++ lib/api.ts
@@ -1,16 +1,16 @@
-import axios from "axios";
+// import axios from "axios";
 
-const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL; // substitua pelo seu
-const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+// const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL; // substitua pelo seu
+// const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
-export const api = axios.create({
-  baseURL: `${SUPABASE_URL}/rest/v1`,
-  headers: {
-    apikey: SUPABASE_ANON_KEY,
-    Authorization: `Bearer ${SUPABASE_ANON_KEY}`,
-    'Content-Type': 'application/json',
-    Prefer: 'return=representation',
-  },
-});
+// export const api = axios.create({
+//   baseURL: `${SUPABASE_URL}/rest/v1`,
+//   headers: {
+//     apikey: SUPABASE_ANON_KEY,
+//     Authorization: `Bearer ${SUPABASE_ANON_KEY}`,
+//     'Content-Type': 'application/json',
+//     Prefer: 'return=representation',
+//   },
+// });
 
 
